e25c91f86997af8679c8279277e981695c2199c1
Dario Traverso <traversos@gmail.com>
2016-01-20 18:08:36 -0800

    Updated Widgetset XML to reference CustomeThemeLoader, and move it to required package naming scheme required by GWT

---
+++ src/main/java/com/vaadin/demo/dashboard/client/CustomThemeLoader.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vaadin.demo.dashboard.client;
+
+import com.vaadin.addon.touchkit.gwt.client.ThemeLoader;
+
+/**
+ *
+ * @author dtraverso
+ */
+public class CustomThemeLoader extends ThemeLoader{
+    
+    @Override
+    public void load(){
+        // do nothing
+    } 
+}
